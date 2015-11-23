93a3f74a32b20f22dd6bd20a992b7d946259ccf2
Doug James <vbdjames@gmail.com>
2015-11-22 20:04:23 -0500

    exercise 1

--- week7-week7_01.Smileys/src/Smileys.java
+++ week7-week7_01.Smileys/src/Smileys.java
@@ -3,9 +3,36 @@ public class Smileys {
 
     public static void main(String[] args) {
         // Test your method at least with the following
-        // printWithSmileys("Method");
-        // printWithSmileys("Beerbottle");
-        // printWithSmileys("Interface");
+         printWithSmileys("Method");
+         printWithSmileys("Beerbottle");
+         printWithSmileys("Interface");
     }
+    
+    private static void printWithSmileys(String characterString) {
+        String content = buildContent(characterString);
+        int smileyCount = content.length() / 2;
+        
+        printSmileys(smileyCount);
+        System.out.println(content);
+        printSmileys(smileyCount);
+        
+    }
+
+    private static String buildContent(String characterString) {
+        String content = ":) " + characterString;
+        if (characterString.length() % 2 == 1) {
+            content += " ";
+        }
+        content += " :)";
+        return content;
+    }
+    
+    private static void printSmileys(int number) {
+        for (int i = 0; i < number; i++) {
+            System.out.print(":)");
+        }
+        System.out.println("");
+    }
+    
 
 }
